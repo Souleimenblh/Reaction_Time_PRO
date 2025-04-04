3bc360e611cc71d9089758a2bb3693f4ec4d6631
Souleimen belhadj <souleimen366@gmail.com>
2025-04-04 10:26:05 +0100

    Create App.jsx

---
+++ App.jsx
@@ -0,0 +1,11 @@
+import ReactionGame from "./components/ReactionGame";
+
+function App() {
+  return (
+    <div className="min-h-screen bg-black text-white">
+      <ReactionGame />
+    </div>
+  );
+}
+
+export default App;
